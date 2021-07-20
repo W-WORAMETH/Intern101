0e65248ce6a5dba42ed8d18a2209c3c906cd7c6f
W-Worameth <Worameth.N@outlook.com>
2021-07-20 21:47:31 +0700

    debug

--- scripts/RpiControl.py
+++ scripts/RpiControl.py
@@ -65,7 +65,8 @@ def callbackSensor(Dataset):
             print(Dataset)
             button = Dataset.data[19]
             print(button)
-            state = Dataset.data[2] - OldDataset.data[2]  
+            print(Dataset.data[2])
+            #state = Dataset.data[2] - OldDataset.data[2]  
             inputcmd = state   #use state because want rising adge
             if inputcmd ==1 :  #rising adge occure
                 toggleSolenoid(button)
