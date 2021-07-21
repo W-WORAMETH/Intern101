6a5b5dad6cfbcf9fbd333c0bdaa99dfc3b67dbef
W-Worameth <Worameth.N@outlook.com>
2021-07-22 05:43:15 +0700

    ..

--- scripts/generateCPG.py
+++ scripts/generateCPG.py
@@ -24,7 +24,7 @@ BiasH1 = 0.0
 BiasH2 = 0.0
 
 rospy.init_node('generateCPG', anonymous=True)
-rate = rospy.Rate(0.5)
+rate = rospy.Rate(0.01)
 def cleanAndExit():
     print("Cleaning...")    
     print("Stop Working ...")
