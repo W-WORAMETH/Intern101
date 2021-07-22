caca6b88eeede129537b9b96f81c65134ce2e54d
W-Worameth <Worameth.N@outlook.com>
2021-07-22 12:29:35 +0700

    xx

--- scripts/generateCPG.py
+++ scripts/generateCPG.py
@@ -5,10 +5,10 @@ import rospy
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Bool
 MI = 0.01
-WeightH1_H1 = 1.4
-WeightH1_H2 = 0.18 + MI
-WeightH2_H2 = 1.4
-WeightH2_H1 = -(0.18 + MI) 
+WeightH1_H1 = 1
+WeightH1_H2 = 0.01 + MI
+WeightH2_H2 = 1
+WeightH2_H1 = -(0.01 + MI) 
 activityH1 = 0
 activityH2 = 0
 outputH1 = 0.01
