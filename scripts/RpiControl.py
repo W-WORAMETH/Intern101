2b1a24f47339f6f55e865a99cabacc91bc43c2db
W-Worameth <Worameth.N@outlook.com>
2021-07-22 09:30:22 +0700

    2

--- scripts/RpiControl.py
+++ scripts/RpiControl.py
@@ -211,7 +211,7 @@ def callbackSensor(Dataset):
 def listener():
     triggerCPG('trigger',trigger)
     #rospy.Subscriber('joyStick',Int16MultiArray, callbackSensor) 
-    rospy.Subscriber('CPG',Float32MultiArray, callbackCPG)       
+    #rospy.Subscriber('CPG',Float32MultiArray, callbackCPG)       
     rospy.spin()
 
 if __name__ == '__main__':
