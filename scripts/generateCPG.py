b502f55b8a1840b9b825e7ceb66251c36573396b
W-Worameth <Worameth.N@outlook.com>
2021-07-22 12:31:14 +0700

    xx

--- scripts/generateCPG.py
+++ scripts/generateCPG.py
@@ -4,15 +4,15 @@ import sys
 import rospy
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Bool
-MI = 0.01
-WeightH1_H1 = 1
-WeightH1_H2 = 0.01 + MI
-WeightH2_H2 = 1
-WeightH2_H1 = -(0.01 + MI) 
+MI = 0.05  
+WeightH1_H1 = 1.4
+WeightH1_H2 = 0.18 + MI
+WeightH2_H2 = 1.4
+WeightH2_H1 = -(0.18 + MI) 
 activityH1 = 0
 activityH2 = 0
-outputH1 = 0.01
-outputH2 = 0.01
+outputH1 = 0.001
+outputH2 = 0.001
 
 Massage = Float64MultiArray()
 Massage.data = []
@@ -39,15 +39,15 @@ def restartCPG():
     global outputH2
 
 
-    MI = 0.01
+    MI = 0.05
     WeightH1_H1 = 1.4
     WeightH1_H2 = 0.18 + MI
 
     WeightH2_H1 = -(0.18 + MI) 
     activityH1 = 0
     activityH2 = 0
-    outputH1 = 0.01
-    outputH2 = 0.01
+    outputH1 = 0.001
+    outputH2 = 0.001
 
 def cleanAndExit():
     print("Cleaning...")    
