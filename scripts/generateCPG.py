9a429d0bd8c730da130a79b5e94fe41bca187579
W-Worameth <Worameth.N@outlook.com>
2021-07-22 11:45:11 +0700

    00

--- scripts/generateCPG.py
+++ scripts/generateCPG.py
@@ -4,7 +4,7 @@ import sys
 import rospy
 from std_msgs.msg import Float64MultiArray
 from std_msgs.msg import Bool
-MI = 1      
+MI = 0.00002      
 WeightH1_H1 = 1.4
 WeightH1_H2 = 0.18 + MI
 WeightH2_H2 = 1.4
@@ -39,7 +39,7 @@ def restartCPG():
     global outputH2
 
 
-    MI = 1
+    MI = 0.00002
     WeightH1_H1 = 1.4
     WeightH1_H2 = 0.18 + MI
     WeightH2_H2 = 1.4
