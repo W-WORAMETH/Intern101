0d75d9eec11d0618ac7829998b15ac207a9aa761
W-Worameth <Worameth.N@outlook.com>
2021-07-22 10:07:56 +0700

    3

--- scripts/RpiControl.py
+++ scripts/RpiControl.py
@@ -172,17 +172,21 @@ def callbackSensor(Dataset):
                 toggleChannal(button)
         OldDataset.data = Dataset.data    
          
-    # if(Dataset.data[19] == 7):  #! must be edit
-    #     print("receive1")
-    #     button = Dataset.data[19]
-    #     inputcmd = Dataset.data[button]
-    #     if inputcmd == -32767: #! must be edit
-    #         print("receive2")   
-    #         trigger = True
-    #         sequenceRobotForward()
-    #     elif inputcmd == 0: #! must be edit
-    #         print("receive3")
-    #         trigger = False
+
+
+
+         
+    if(Dataset.data[19] == 7):  #! must be edit
+        print("receive1")
+        button = Dataset.data[19]
+        inputcmd = Dataset.data[button]
+        if inputcmd == -32767: #! must be edit
+            print("receive2")   
+            trigger = True
+            sequenceRobotForward()
+        elif inputcmd == 0: #! must be edit
+            print("receive3")
+            trigger = False
         
        
     
