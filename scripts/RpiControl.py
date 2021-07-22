291056ecf8aedac061558dcc03978fe0f7b541f4
W-Worameth <Worameth.N@outlook.com>
2021-07-22 20:00:36 +0700

    10

--- scripts/RpiControl.py
+++ scripts/RpiControl.py
@@ -129,8 +129,6 @@ def callbackCPG(CPG):
     print("FrontCPG = "+str(FrontCPG) )
     print("BackCPG = "+str(BackCPG) )
 
-    pass
-
 def sequenceRobotForward() :
     global Solenoid
     global FrontCPG
@@ -195,22 +193,22 @@ def callbackJoy(Dataset):
             if inputcmd == 1 :  #rising adge occure
                 print("prp toggle")
                 toggleChannal(button)
-        OldDataset.data = Dataset.data    
+         
      
          
-        if(Dataset.data[19] == 7):  #! must be edit
+        else if(Dataset.data[19] == 7):  #! must be edit
             print("receive1")
             button = Dataset.data[19]
             inputcmd = Dataset.data[button]
             print(Dataset.data[button])
-            if inputcmd == -32767: #! must be edit
+            if(inputcmd == -32767): #! must be edit
                 print("receive2")   
                 trigger = True
-            elif inputcmd == 0: #! must be edit
+            elif(inputcmd == 0): #! must be edit
                 print("receive3")
                 trigger = False
                 
-        
+        OldDataset.data = Dataset.data   
 
 
             
