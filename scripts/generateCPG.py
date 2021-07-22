d34225d7a6a0c302b591a3371c22c99c31bdad50
W-Worameth <Worameth.N@outlook.com>
2021-07-22 17:36:47 +0700

    99

--- scripts/generateCPG.py
+++ scripts/generateCPG.py
@@ -55,8 +55,8 @@ def restartCPG():
     WeightH2_H1 = -(0.18 + MI) 
     activityH1 = 0
     activityH2 = 0
-    outputH1 = 0.01
-    outputH2 = 0.01
+    outputH1 = 0.00001
+    outputH2 = 0.00001
 
 def cleanAndExit():
     print("Cleaning...")    
