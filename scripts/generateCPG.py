a4f92d838bde8a8481753053520b83916d2995e2
W-Worameth <Worameth.N@outlook.com>
2021-07-26 21:59:19 +0700

    1cpg

--- scripts/generateCPG.py
+++ scripts/generateCPG.py
@@ -91,7 +91,7 @@ def generateCPG() :
     outputH1 = math.tanh(activityH1)
     outputH2 = math.tanh(activityH2)
 
-    output.data = [outputH1, -outputH1]
+    output.data = [outputH1, outputH2]
     rate.sleep()
     
     
