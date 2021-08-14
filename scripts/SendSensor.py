16081c4a92d2c30167483c68397f053e6a1ed886
W-Worameth <Worameth.N@outlook.com>
2021-08-14 11:42:00 +0700

    ..

--- scripts/SendSensor.py
+++ scripts/SendSensor.py
@@ -28,42 +28,42 @@ def cleanAndExit():
     sys.exit()
 
 
-# hx1 = HX711(6,5)
-# hx2 = HX711(19,13)
-# hx3 = HX711(21,26)
-# hx4 = HX711(16,20)
+hx1 = HX711(6,5)
+hx2 = HX711(19,13)
+hx3 = HX711(21,26)
+hx4 = HX711(16,20)
 
 print("done initial ...")
 
-# hx1.set_reading_format("MSB", "MSB")
-# hx2.set_reading_format("MSB", "MSB")
-# hx3.set_reading_format("MSB", "MSB")
-# hx4.set_reading_format("MSB", "MSB")
+hx1.set_reading_format("MSB", "MSB")
+hx2.set_reading_format("MSB", "MSB")
+hx3.set_reading_format("MSB", "MSB")
+hx4.set_reading_format("MSB", "MSB")
 
 
-# hx1.set_reference_unit(1)
-# hx2.set_reference_unit(1)
-# hx3.set_reference_unit(1)
-# hx4.set_reference_unit(1)
+hx1.set_reference_unit(1)
+hx2.set_reference_unit(1)
+hx3.set_reference_unit(1)
+hx4.set_reference_unit(1)
 
 
-# hx1.reset()
-# hx2.reset()
-# hx3.reset()
-# hx4.reset()
+hx1.reset()
+hx2.reset()
+hx3.reset()
+hx4.reset()
 
 # to use both channels, you'll need to tare them both
 #hx.tare_A()
 #hx.tare_B()
 
 def ReadSensor():
-    # val1 = hx1.get_weight()
-    # val2 = hx2.get_weight()
+    val1 = hx1.get_weight()
+    val2 = hx2.get_weight()
     # val3 = hx3.get_weight()
     # val4 = hx4.get_weight()
 
-    # rospy.loginfo(rospy.get_caller_id() + "   sensor1 = %s", str(val1))
-    # rospy.loginfo(rospy.get_caller_id() + "   sensor2 = %s", str(val2))
+    rospy.loginfo(rospy.get_caller_id() + "   sensor1 = %s", str(val1))
+    rospy.loginfo(rospy.get_caller_id() + "   sensor2 = %s", str(val2))
     # rospy.loginfo(rospy.get_caller_id() + "   sensor3 = %s", str(val3))
     # rospy.loginfo(rospy.get_caller_id() + "   sensor4 = %s", str(val4))
 
@@ -78,12 +78,12 @@ def ReadSensor():
     if(not rospy.is_shutdown()): 
         sendData('sensor',Data)
 
-    # hx1.reset()
-    # hx2.reset()
-    #hx1.power_down()
-    #hx1.power_up()
-    #hx2.power_down()
-    #hx2.power_up()
+    hx1.reset()
+    hx2.reset()
+    hx1.power_down()
+    hx1.power_up()
+    hx2.power_down()
+    hx2.power_up()
     # hx3.power_down()
     # hx3.power_up()
     # hx4.power_down()
